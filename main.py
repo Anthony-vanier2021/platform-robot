73c5ccfa6aa4f00ff08e23cda19fe19e376219dc
Anthony-vanier2021 <2149532@edu.vaniercollege.qc.ca>
2023-12-19 14:46:44 -0500

    Remote Hello Test

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import config, time
 from machine import Pin, PWM
 
 print("Setting Pins")
-ms1 = Pin(config.MS1_PINNUM, mode=Pin.OUT, value=0)
-ms2 = Pin(config.MS2_PINNUM, mode=Pin.OUT, value=0)
-ms3 = Pin(config.MS3_PINNUM, mode=Pin.OUT, value=0)
+ms1 = Pin(config.MS1_PINNUM, mode=Pin.OUT, value=1)
+ms2 = Pin(config.MS2_PINNUM, mode=Pin.OUT, value=1)
+ms3 = Pin(config.MS3_PINNUM, mode=Pin.OUT, value=1)
 step = Pin(config.STEP_PINNUM, mode=Pin.OUT)
 rsth = Pin(config.RSTH_PINNUM, mode=Pin.OUT, value=1)
 slph = Pin(config.SLPH_PINNUM, mode=Pin.OUT, value=1)
-dir1 = Pin(config.DIR2_PINNUM, mode=Pin.OUT, value=0)
+dir1 = Pin(config.DIR2_PINNUM, mode=Pin.OUT, value=1)
 #print("Setting frequency to 10Hz")
 #step.freq(10)    #10Hz frequency
     #time.sleep_us(500)
@@ -21,6 +21,6 @@ for i in range(abs(num_step)):
     step.value(1)
     time.sleep_us(20)
     step.value(0)
-    time.sleep_us(20)
+    time.sleep_us(20000)
 
 print("End")
