18dcd8c6750e22d10a14b3813101d55549b7891f
Anthony-vanier2021 <2149532@edu.vaniercollege.qc.ca>
2023-12-19 15:01:38 -0500

    Remote Hello World

---
+++ Socket.py
@@ -0,0 +1,26 @@
+#Anthony Tremblay - Socket Programming
+import socket, config
+
+bufferSize = 1024
+serverPort = 2001
+
+try:
+    UDPserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print("Successful Socket Created")
+except socket.error as error:
+    print("ERROR: Socket error encountered: %s"%(error))
+
+UDPserver.bind((config.IP_ADDRESS,serverPort))
+print(f"Server ready @ {config.IP_ADDRESS}:{serverPort}")
+
+while True:
+    clientMsg, returnDest = UDPserver.recvfrom(bufferSize)
+    data = clientMsg.decode() 
+    returnIP = returnDest[0]
+    print(data)
+    if data == "END":
+        break;
+
+print("Goodbye!")
+UDPserver.close()
+
