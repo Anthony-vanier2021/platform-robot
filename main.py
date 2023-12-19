dfe10cd4efed0967d420aed40b237f3fd3627e8e
Anthony-vanier2021 <2149532@edu.vaniercollege.qc.ca>
2023-12-19 14:38:24 -0500

    Remote Hello Test

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-#Anthony Tremblay - Socket Programming
-import socket, config
+#Remote Hello World - Should Turn West Motor, but doesn't work
+import config, time
+from machine import Pin, PWM
 
-bufferSize = 1024
-serverPort = 2001
+print("Setting Pins")
+ms1 = Pin(config.MS1_PINNUM, mode=Pin.OUT, value=0)
+ms2 = Pin(config.MS2_PINNUM, mode=Pin.OUT, value=0)
+ms3 = Pin(config.MS3_PINNUM, mode=Pin.OUT, value=0)
+step = Pin(config.STEP_PINNUM, mode=Pin.OUT)
+rsth = Pin(config.RSTH_PINNUM, mode=Pin.OUT, value=1)
+slph = Pin(config.SLPH_PINNUM, mode=Pin.OUT, value=1)
+dir1 = Pin(config.DIR2_PINNUM, mode=Pin.OUT, value=0)
+#print("Setting frequency to 10Hz")
+#step.freq(10)    #10Hz frequency
+    #time.sleep_us(500)
 
-try:
-    UDPserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    print("Successful Socket Created")
-except socket.error as error:
-    print("ERROR: Socket error encountered: %s"%(error))
+num_step = 100
+dir1.value(0 if num_step > 0 else 1)
+print("Entering loop")
+for i in range(abs(num_step)):
+    step.value(1)
+    time.sleep_us(20)
+    step.value(0)
+    time.sleep_us(20)
 
-UDPserver.bind((config.IP_ADDRESS,serverPort))
-print(f"Server ready @ {config.IP_ADDRESS}:{serverPort}")
-
-while True:
-    clientMsg, returnDest = UDPserver.recvfrom(bufferSize)
-    data = clientMsg.decode() 
-    returnIP = returnDest[0]
-    print(data)
-    if data == "END":
-        break;
-
-print("Goodbye!")
-UDPserver.close()
-
+print("End")
