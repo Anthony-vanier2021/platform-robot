cc4d33b5c8550ec2ca43196f9271b5e69726ec9a
Anthony-vanier2021 <2149532@edu.vaniercollege.qc.ca>
2023-12-19 10:10:36 -0500

    Tweaking values (idk wtf is happening)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("Setting Pins")
 ms1 = Pin(config.MS1_PINNUM, mode=Pin.OUT, value=0)
 ms2 = Pin(config.MS2_PINNUM, mode=Pin.OUT, value=0)
 ms3 = Pin(config.MS3_PINNUM, mode=Pin.OUT, value=0)
-step = PWM(Pin(config.STEP_PINNUM, mode=Pin.OUT))
+step = Pin(config.STEP_PINNUM, mode=Pin.OUT)
 rsth = Pin(config.RSTH_PINNUM, mode=Pin.OUT, value=1)
 slph = Pin(config.SLPH_PINNUM, mode=Pin.OUT, value=1)
 dir1 = Pin(config.DIR1_PINNUM, mode=Pin.OUT, value=0)
@@ -14,13 +14,13 @@ print("Setting frequency to 10Hz")
 step.freq(10)    #10Hz frequency
 
 print("Entering loop")
-for i in range(2):
+for i in range(100):
     step.duty(512)
-    time.sleep_ms(500)
+    time.sleep_us(500)
     step.duty(0)
     dir1.value(1)
     step.duty(512)
-    time.sleep_ms(500)
+    time.sleep_us(500)
     step.duty(0)
     print(i)
 
