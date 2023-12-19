aeb84d2e9330e6ca034e1123e06a883c9d991652
Anthony-vanier2021 <2149532@edu.vaniercollege.qc.ca>
2023-12-19 12:30:07 -0500

    Socket Programming

---
+++ Controller.py
@@ -0,0 +1,14 @@
+import socket
+
+Dest_IP = '192.168.206.154' # Get IP from serial connection
+Dest_Port = 2001                    #server port
+Destination = (Dest_IP,Dest_Port)   #server full address including port
+UDPclient = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Opening socket
+
+ClientMsg = input("Please Enter Message to ESP32: ")
+Msg2Server = str.encode(ClientMsg)      #encode message for network communication
+UDPclient.sendto(Msg2Server,Destination)    #send message to server
+print(f"Sending {ClientMsg} to ESP32")
+if ClientMsg != "END":
+    print("Goodbye!")
+    UDPclient.close()   #closing socket
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-#Remote Hello World - Turns West Motor
-import config, time
-from machine import Pin, PWM
+#Anthony Tremblay - Socket Programming
+import socket, config
 
-print("Setting Pins")
-ms1 = Pin(config.MS1_PINNUM, mode=Pin.OUT, value=0)
-ms2 = Pin(config.MS2_PINNUM, mode=Pin.OUT, value=0)
-ms3 = Pin(config.MS3_PINNUM, mode=Pin.OUT, value=0)
-step = Pin(config.STEP_PINNUM, mode=Pin.OUT)
-rsth = Pin(config.RSTH_PINNUM, mode=Pin.OUT, value=1)
-slph = Pin(config.SLPH_PINNUM, mode=Pin.OUT, value=1)
-dir1 = Pin(config.DIR1_PINNUM, mode=Pin.OUT, value=0)
-#print("Setting frequency to 10Hz")
-#step.freq(10)    #10Hz frequency
-    #time.sleep_us(500)
-    
-print("Entering loop")
-for i in range(1000):
-    step.on()
-    time.sleep_us(100)
-    step.off()
-    time.sleep_us(100)
-    print(i)
+bufferSize = 1024
+serverPort = 2001
 
-print("End")
+try:
+    UDPserver = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    print("Successful Socket Created")
+except socket.error as error:
+    print("ERROR: Socket error encountered: %s"%(error))
+
+UDPserver.bind((config.IP_ADDRESS,serverPort))
+print(f"Server ready @ {config.IP_ADDRESS}:{serverPort}")
+
+while True:
+    clientMsg, returnDest = UDPserver.recvfrom(bufferSize)
+    data = clientMsg.decode() 
+    returnIP = returnDest[0]
+    print(data)
+    if data == "END":
+        break;
+
+UDPserver.close()
+
